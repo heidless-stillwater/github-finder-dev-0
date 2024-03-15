6e7c7349a4ada842533b43ed15e61dcb69713d9e
heidless <rob.lockhart@yahoo.co.uk>
2024-03-15 10:44:04 +0000

    alert component complete

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 import NavBar from './components/layout/NavBar'
 import Footer from './components/layout/Footer'
+import Alert from './components/layout/Alert'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
+
       <Router>
         <div className='flex flex-col justify-between h-screen'>
           <NavBar />
 
           <main className='container mx-auto px-3 pb-12'>
+            <Alert />
+            
             <Routes>
               <Route
                 path='/'
@@ -33,6 +39,8 @@ function App() {
           <Footer />
         </div>
       </Router>
+      </AlertProvider>
+
     </GithubProvider>
   )
 }
